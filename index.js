679852084868d4bb6a3401471429c604e80ab3b4
Doriano <doriano.bergamin18@gmail.com>
2024-01-24 23:02:48 +1100

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution in this file!
+const employee = {
+    name: "valueNAme",
+    streetAddress:"valueStreet",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+      };
+    }
+
+    const newEmployee = updateEmployeeWithKeyAndValue(
+        employee,
+        "name",
+        "newValueName",
+    )
+
+    
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value; 
+    return obj;
+}
+
+  const newEmployee1 = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee, 
+    "name", 
+    "newValueName");
+
+    function deleteFromEmployeeByKey(obj, key, value){
+        return {
+            ...obj,
+            [key]: value,
+          };
+    }
+delete employee.name;
+
+    function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+        obj[key] = value; 
+        return obj;
+    }
+    delete employee.name;
+  
